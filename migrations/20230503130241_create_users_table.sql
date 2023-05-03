e407091deaabfad0f502787069b0b7bc487b368d
Александр Ратуев <a.ratuev@tapir.ws>
2023-05-03 19:12:07 +0300

    task-1 модель User

---
+++ migrations/20230503130241_create_users_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users
+(
+    id         serial PRIMARY KEY,
+    email      text      NOT NULL UNIQUE,
+    username   text      NOT NULL,
+    password   text      NOT NULL,
+    role       text      NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
