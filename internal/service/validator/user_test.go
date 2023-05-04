dbc4151e018e228d2f8c7483749ba97be3ad4dee
Александр Ратуев <a.ratuev@tapir.ws>
2023-05-04 19:51:58 +0300

    task-1 тест валидатора

---
+++ internal/service/validator/user_test.go
@@ -0,0 +1,31 @@
+package validator_test
+
+import (
+	"github.com/a13hander/auth-service-api/internal/domain/usecase"
+	"github.com/a13hander/auth-service-api/internal/service/validator"
+	"testing"
+)
+
+func testPassConfirmCheck(t *testing.T) {
+	sut := validator.NewUserValidator()
+
+	request := &usecase.CreateUserRequest{
+		Password:        "123",
+		PasswordConfirm: "123",
+	}
+
+	err := sut.ValidateCreating(request)
+	if err != nil {
+		t.Fail()
+	}
+
+	request = &usecase.CreateUserRequest{
+		Password:        "231",
+		PasswordConfirm: "123",
+	}
+
+	err = sut.ValidateCreating(request)
+	if err == nil {
+		t.Fail()
+	}
+}
