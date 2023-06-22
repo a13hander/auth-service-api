8635bf3eb8531287e6e0c1ef836105a0b0531150
Александр Ратуев <a.ratuev@tapir.ws>
2023-06-22 11:05:45 +0300

    task-5 исправление в тестах

--- internal/domain/validator/user_test.go
+++ internal/domain/validator/user_test.go
@@ -1,6 +1,7 @@
 package validator_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/a13hander/auth-service-api/internal/domain/usecase"
@@ -11,21 +12,21 @@ func TestPassConfirmCheck(t *testing.T) {
 	sut := validator.NewUserValidator()
 
 	request := &usecase.CreateUserRequest{
-		Password:        "123",
-		PasswordConfirm: "123",
+		Password:        "1234567890",
+		PasswordConfirm: "1234567890",
 	}
 
-	err := sut.ValidateCreating(request)
+	err := sut.ValidateCreating(context.Background(), request)
 	if err != nil {
 		t.Fail()
 	}
 
 	request = &usecase.CreateUserRequest{
-		Password:        "231",
-		PasswordConfirm: "123",
+		Password:        "1234567890",
+		PasswordConfirm: "0234567891",
 	}
 
-	err = sut.ValidateCreating(request)
+	err = sut.ValidateCreating(context.Background(), request)
 	if err == nil {
 		t.Fail()
 	}
